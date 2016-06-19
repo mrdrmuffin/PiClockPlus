97c1655c432aeb3f05813d8f176023ea9b1894f7
mrdrmuffin <mrdrmuffin@gmail.com>
2016-06-19 10:32:22 -0400

    Adding Transit Info
    Gets some basic data from the transit feed, displays it on screen. This
    isn't the data we really want, but just a placeholder.

--- app.py
+++ app.py
@@ -8,7 +8,13 @@ import io
 
 from PIL import Image, ImageTk
 
-appid = '92ce933d50001c4176501088ed8e1d7c'
+import xml.etree.ElementTree as etree
+
+import ipdb
+
+id_file = open('tokens.txt')
+weather_key = id_file.readline().split('=')[1]
+transit_key = id_file.readline().split('=')[1]
 
 
 class WeatherInfo:
@@ -31,7 +37,7 @@ class WeatherInfo:
 
     def update_weather_data(self):
         conn = httplib.HTTPConnection('api.openweathermap.org')
-        conn.request("GET", "/data/2.5/weather?id=5125771&units=imperial&APPID=%s" % appid)
+        conn.request("GET", "/data/2.5/weather?id=5125771&units=imperial&APPID=%s" % weather_key)
         response = conn.getresponse()
         response_raw = response.read()
         response.close()
@@ -54,11 +60,44 @@ class WeatherInfo:
         self.description = response['weather'][0]['description']
         self.icon = response['weather'][0]['icon']
 
+        print(self.icon)
+
         self.load_weather_image()
 
         # Get the weather data every 10 and a half minutes
         # threading.Timer(630, update_weather_data).start()
         return self.temp
+
+
+class TransitInfo:
+    def __init__(self):
+        self.track = '102A'
+        self.time = '8:07 AM'
+        self.destination = 'New Haven'
+        self.dest_id = 116
+        self.origin_id = 1
+        self.valid_routes = [3, 4]  # New Haven, New Canaan
+
+    def update_data(self):
+        conn = httplib.HTTPConnection('mnorth.prod.acquia-sites.com')
+        conn.request("GET", "/wse/gtfsrtwebapi/v2/gtfsrt/%s/getfeed" % transit_key)
+        response = conn.getresponse()
+        response_raw = response.read()
+        response.close()
+        conn.close()
+        print(response_raw)
+
+        response = json.loads(response_raw.decode("utf-8"))
+        print(json.dumps(response, indent=4))
+        trip_updates = response['entity']
+        for trip_update in trip_updates:
+            print(trip_update)
+            if trip_update['trip_update']['trip']['route_id'] in self.valid_routes:
+                self.time = trip_update['trip']['start_time']
+
+
+        # ipdb.set_trace()
+        return 'Hi'
 
 
 def tick():
@@ -83,8 +122,10 @@ def update_weather():
     weather_image.config(image=wi.image)
     #self.widget.config(image=image, text='Sunny as fuck')
 
-    weather.after(30000, update_weather)
+    weather.after(60000, update_weather)
 
+tinfo = TransitInfo()
+tinfo.update_data()
 
 root = Tk()
 root.geometry('480x320')
@@ -95,12 +136,16 @@ time1 = ''
 clock = Label(root, font=('times', 48, 'bold'), bg='DeepSkyBlue4', fg='white')
 clock.pack(fill=BOTH, expand=1)
 
+transit = Label(root, text="Train %s" % str(tinfo.time), font=('times', 20, 'bold'), bg='DeepSkyBlue4', fg='white')
+transit.pack(fill=BOTH)
+
 wi = WeatherInfo()
 weather = Label(root, text="Hello NYC! %s" % wi.temp, font=('times', 20, 'bold'), bg='DeepSkyBlue4', fg='white')
 weather.pack(side=LEFT)
 
 weather_image = Label(root, bg='DeepSkyBlue4')
 weather_image.pack(side=RIGHT)
+
 
 # root.bind('<KeyPress>', onKeyPress)       #For example, bind the onKeyPress method (you must create it), and have some code
 # done when key is pressed
